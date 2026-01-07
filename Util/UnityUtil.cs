252d2cb9792c5a36cdcf8cb90d98ee9919e45d8c
9thCore <38700349+9thCore@users.noreply.github.com>
2026-01-07 21:50:30 +0200

    Convenient Unity utility

---
+++ Util/UnityUtil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace RDEditorPlus.Util
+{
+    public static class UnityUtil
+    {
+        public static C EnsureComponent<C>(this GameObject gameObject) where C : Component
+        {
+            if (gameObject.TryGetComponent(out C component))
+            {
+                return component;
+            }
+
+            return gameObject.AddComponent<C>();
+        }
+
+        public static void AnchorRelativeY(this RectTransform transform, float delta)
+        {
+            transform.AnchorPosY(transform.anchoredPosition.y + delta);
+        }
+    }
+}
