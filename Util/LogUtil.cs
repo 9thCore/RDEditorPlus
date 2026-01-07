8aa310373d2551938c4b089b331f7dcd7ef75caf
9thCore <38700349+9thCore@users.noreply.github.com>
2026-01-07 21:50:58 +0200

    Convenient logging util (though its not used)

---
+++ Util/LogUtil.cs
@@ -0,0 +1,16 @@
+﻿using RDLevelEditor;
+
+namespace RDEditorPlus.Util
+{
+    public static class LogUtil
+    {
+        public static string Event(LevelEvent_Base levelEvent)
+        {
+            if (levelEvent.usesY)
+            {
+                return $"{levelEvent.type}@(Bar: {levelEvent.bar}, Beat: {levelEvent.beat}, y: {levelEvent.y})";
+            }
+            return $"{levelEvent.type}@(Bar: {levelEvent.bar}, Beat: {levelEvent.beat}, target: {levelEvent.target})";
+        }
+    }
+}
