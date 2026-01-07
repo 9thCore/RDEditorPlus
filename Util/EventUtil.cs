8fb44c031a0a88cb2a60826db28cd48fb2cd59bc
9thCore <38700349+9thCore@users.noreply.github.com>
2026-01-07 21:51:11 +0200

    To-be convenient RD Event utility

---
+++ Util/EventUtil.cs
@@ -0,0 +1,41 @@
+﻿using RDLevelEditor;
+
+namespace RDEditorPlus.Util
+{
+    public static class EventUtil
+    {
+        public static bool IsSpriteEvent(this LevelEvent_Base levelEvent)
+        {
+            return levelEvent.isSpriteTabEvent;
+        }
+
+        // Dummy
+        public static bool IsSoundEvent(this LevelEvent_Base levelEvent)
+        {
+            return false;
+        }
+
+        public static bool IsRowEvent(this LevelEvent_Base levelEvent)
+        {
+            return levelEvent.isClassicRowEvent || levelEvent.isOneshotRowEvent;
+        }
+
+        // Dummy
+        public static bool IsRoomEvent(this LevelEvent_Base levelEvent)
+        {
+            return false;
+        }
+
+        // Dummy
+        public static bool IsActionEvent(this LevelEvent_Base levelEvent)
+        {
+            return false;
+        }
+
+        // Dummy
+        public static bool IsWindowEvent(this LevelEvent_Base levelEvent)
+        {
+            return false;
+        }
+    }
+}
