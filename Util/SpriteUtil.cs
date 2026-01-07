939cf5fecc841ff9661b28ce04fe9b4406ad7ba4
9thCore <38700349+9thCore@users.noreply.github.com>
2026-01-07 21:50:38 +0200

    Conveneint RD sprite util

---
+++ Util/SpriteUtil.cs
@@ -0,0 +1,40 @@
+﻿using RDLevelEditor;
+
+namespace RDEditorPlus.Util
+{
+    public static class SpriteUtil
+    {
+        public static bool TryGetSprite(string target, out LevelEvent_MakeSprite sprite)
+        {
+            foreach (LevelEvent_MakeSprite makeSprite in scnEditor.instance.spritesData)
+            {
+                if (makeSprite.target == target)
+                {
+                    sprite = makeSprite;
+                    return true;
+                }
+            }
+
+            sprite = null;
+            return false;
+        }
+
+        public static bool TryGetRowInRoom(string target, int room, out int row)
+        {
+            row = 0;
+
+            foreach (LevelEvent_MakeSprite sprite in scnEditor.instance.spritesData)
+            {
+                if (sprite.target == target)
+                {
+                    return true;
+                } else if (sprite.room == room)
+                {
+                    row += 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
