b8a514e3db7150b7f03167dccef060cf516632f6
9thCore <38700349+9thCore@users.noreply.github.com>
2026-01-24 07:16:18 +0200

    Revert "Remove deprecated autocompletion patch"
    Readd it in the form of allowing through known methods if they aren't documented

---
+++ Patch/CustomMethod/Patch_MethodAutocompleteUI.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RDEditorPlus.ExtraData;
+using System.Reflection;
+
+namespace RDEditorPlus.Patch.CustomMethod
+{
+    internal static class Patch_MethodAutocompleteUI
+    {
+        [HarmonyPatch(typeof(MethodAutocompleteUI), nameof(MethodAutocompleteUI.Search))]
+        private static class Search
+        {
+            private static void ILManipulator(ILContext il)
+            {
+                const byte attributeLocalIndex = 25;
+                const byte methodInfoLocalIndex = 24;
+
+                MethodInfo isDev = typeof(RDBase).GetMethod($"get_{nameof(RDBase.isDev)}", BindingFlags.Public | BindingFlags.Static);
+                MethodInfo additionalMethodAllow = typeof(Search).GetMethod(nameof(AdditionalMethodAllow), BindingFlags.NonPublic | BindingFlags.Static);
+
+                ILCursor cursor = new(il);
+
+                cursor.GotoNext(MoveType.After, instruction => instruction.MatchLdloc(attributeLocalIndex))
+                    .Emit(OpCodes.Ldloc_S, methodInfoLocalIndex)
+                    .Emit(OpCodes.Call, additionalMethodAllow);
+            }
+
+            private static bool AdditionalMethodAllow(ListedMethodAttribute attribute, MethodInfo methodInfo)
+            {
+                if (attribute != null)
+                {
+                    return true;
+                }
+
+                return CustomMethodStorage.Instance.IsAllowed(methodInfo);
+            }
+        }
+    }
+}
