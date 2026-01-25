67b42de59cc75fd8a7f4f675fac2cb28053a1940
9thCore <38700349+9thCore@users.noreply.github.com>
2026-01-25 21:05:14 +0200

    Toggle properties for all selected events

--- Patch/Select/MultiEdit/Patch_Property.cs
+++ Patch/Select/MultiEdit/Patch_Property.cs
@@ -53,6 +53,36 @@ namespace RDEditorPlus.Patch.Select.MultiEdit
                     .Emit(OpCodes.Call, delegateInjector);
             }
 
+            private static void Postfix(Property __result)
+            {
+                BasePropertyInfo propertyInfo = __result.propertyInfo;
+
+                if (propertyInfo is NullablePropertyInfo nullablePropertyInfo)
+                {
+                    InspectorPanel inspectorPanel = __result.control.inspectorPanel;
+
+                    __result.button.onClick.RemoveAllListeners();
+                    __result.button.onClick.AddListener(() =>
+                    {
+                        LevelEventControl_Base currentControl = scnEditor.instance.selectedControls[0];
+
+                        bool flag = nullablePropertyInfo.IsUsed(currentControl.levelEvent);
+                        object obj = flag ? null : nullablePropertyInfo.toggleOnValue;
+
+                        foreach (LevelEventControl_Base eventControl in scnEditor.instance.selectedControls)
+                        {
+                            propertyInfo.propertyInfo.SetValue(eventControl.levelEvent, obj);
+                        }
+
+                        scrConductor.PlayImmediately(flag ? "sndButtonClick" : "sndButtonClickOn",
+                            group: RDUtils.GetMixerGroup("LevelEditorInspectorPanel"), ignoreListenerPause: true);
+
+                        __result.UpdateUI(currentControl.levelEvent);
+                        currentControl.SaveAndUpdateUI();
+                    });
+                }
+            }
+
             private static InspectorPanel.ChangeAction InjectDelegate(InspectorPanel.ChangeAction action, GameObject gameObject)
             {
                 return ConstructDelegate(action, gameObject.GetComponent<Property>());
