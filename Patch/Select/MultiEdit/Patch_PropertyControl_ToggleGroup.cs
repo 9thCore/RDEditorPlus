c254ca9e2c9dc4005ecf199b83d3618f06c750df
9thCore <38700349+9thCore@users.noreply.github.com>
2026-01-27 16:39:49 +0200

    Handle ToggleGroup property

---
+++ Patch/Select/MultiEdit/Patch_PropertyControl_ToggleGroup.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RDEditorPlus.Util;
+using RDLevelEditor;
+
+namespace RDEditorPlus.Patch.Select.MultiEdit
+{
+    internal static class Patch_PropertyControl_ToggleGroup
+    {
+        [HarmonyPatch(typeof(PropertyControl_ToggleGroup), nameof(PropertyControl_ToggleGroup.UpdateUI))]
+        private static class UpdateUI
+        {
+            private static bool Prefix(PropertyControl_ToggleGroup __instance)
+            {
+                if (!__instance.EqualValueForSelectedEvents())
+                {
+                    __instance.toggleGroup.SetAllTogglesOff(sendCallback: false);
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
