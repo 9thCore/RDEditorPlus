c3ab30435e6bdc050c21510e29b59481cd56cd9e
9thCore <38700349+9thCore@users.noreply.github.com>
2026-02-13 21:56:36 +0200

    Fully support hide row event

---
+++ Patch/Select/MultiEdit/Patch_InspectorPanel_HideRow.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RDLevelEditor;
+using System.Linq;
+
+namespace RDEditorPlus.Patch.Select.MultiEdit
+{
+    internal static class Patch_InspectorPanel_HideRow
+    {
+        [HarmonyPatch(typeof(InspectorPanel_HideRow), nameof(InspectorPanel_HideRow.UpdateUIProperties))]
+        private static class UpdateUIProperties
+        {
+            private static void ILManipulator(ILContext il)
+            {
+                ILCursor cursor = new(il);
+
+                cursor
+                    .GotoNext(MoveType.After, instruction => instruction.MatchCallvirt<LevelEvent_HideRow>($"get_{nameof(LevelEvent_HideRow.show)}"))
+                    .EmitDelegate((RowVisibilityMode _) =>
+                    {
+                        return scnEditor.instance.selectedControls
+                        .Select(control => control.levelEvent as LevelEvent_HideRow)
+                        .Any(levelEvent => levelEvent != null && levelEvent.show != RowVisibilityMode.Visible);
+                    });
+            }
+        }
+    }
+}
