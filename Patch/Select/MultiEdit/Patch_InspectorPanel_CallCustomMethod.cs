6adb9da34c249280555066364cf00ff3a81d4c68
9thCore <38700349+9thCore@users.noreply.github.com>
2026-02-15 08:14:36 +0200

    Feat: Call Custom Method support

---
+++ Patch/Select/MultiEdit/Patch_InspectorPanel_CallCustomMethod.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using RDEditorPlus.Util;
+using RDLevelEditor;
+using System.Linq;
+
+namespace RDEditorPlus.Patch.Select.MultiEdit
+{
+    internal static class Patch_InspectorPanel_CallCustomMethod
+    {
+        [HarmonyPatch(typeof(InspectorPanel_CallCustomMethod), nameof(InspectorPanel_CallCustomMethod.CheckMethodDesc))]
+        private static class CheckMethodDesc
+        {
+            private static bool Prefix()
+            {
+                if (!InspectorUtil.CanMultiEdit()
+                    || methodInput.EqualValueForSelectedEvents())
+                {
+                    return true;
+                }
+
+                InspectorPanel_CallCustomMethod.ClearMethodDesc();
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(InspectorPanel_CallCustomMethod), nameof(InspectorPanel_CallCustomMethod.Start))]
+        private static class Start
+        {
+            private static void Postfix(InspectorPanel_CallCustomMethod __instance)
+            {
+                methodInput = __instance.properties.Where(property => property.name == nameof(LevelEvent_CallCustomMethod.methodName))
+                    .Select(property => property.control as PropertyControl_InputField)
+                    .First();
+            }
+        }
+
+        private static PropertyControl_InputField methodInput;
+    }
+}
