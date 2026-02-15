4508490779082bde6b42e53fdace62c47ed88130
9thCore <38700349+9thCore@users.noreply.github.com>
2026-02-15 08:42:50 +0200

    Feat: partial support for the hand event
    the dropdown autosaves after setting this but idk what i can realistically do against that since all the other dropdown updates (sprite expression, vfx event) happened "off-site"

---
+++ Patch/Select/MultiEdit/Patch_PropertyControl_Hand.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RDEditorPlus.Util;
+using RDLevelEditor;
+
+namespace RDEditorPlus.Patch.Select.MultiEdit
+{
+    internal static class Patch_PropertyControl_Hand
+    {
+        [HarmonyPatch(typeof(PropertyControl_Hand), nameof(PropertyControl_Hand.UpdateUI))]
+        private static class UpdateUI
+        {
+            private static bool Prefix(PropertyControl_Hand __instance)
+            {
+                if (!__instance.EqualValueForSelectedEvents())
+                {
+                    __instance.hand.SetAllTogglesOff(sendCallback: false);
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
