d65480a6153b3643c69ea126d774ded0d5ab626d
9thCore <38700349+9thCore@users.noreply.github.com>
2026-02-26 21:00:31 +0200

    Add helpful utility
    this probably won't be used in final releases but it's about time i stopped writing the boilerplate all the time

---
+++ Util/ILUtil.cs
@@ -0,0 +1,20 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace RDEditorPlus.Util
+{
+    public static class ILUtil
+    {
+        public static void Print(this Instruction instruction)
+        {
+            if (instruction.Operand is ILLabel label)
+            {
+                Plugin.LogInfo($"{instruction.Offset}: {instruction.OpCode} {label.Target.Offset}");
+            }
+            else
+            {
+                Plugin.LogInfo($"{instruction.Offset}: {instruction.OpCode} {instruction.Operand}");
+            }
+        }
+    }
+}
