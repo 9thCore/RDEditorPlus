e19022926931f318f23e41faf115b9a2e026dcc5
9thCore <38700349+9thCore@users.noreply.github.com>
2026-03-16 18:40:55 +0200

    Patch: MultiEdit NRE if editing a sound event

--- Patch/Select/MultiEdit/Patch_Conditionals.cs
+++ Patch/Select/MultiEdit/Patch_Conditionals.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using RDEditorPlus.Functionality.MultiEdit;
 using RDEditorPlus.Util;
 using RDLevelEditor;
 using System.Linq;
@@ -19,65 +20,8 @@ namespace RDEditorPlus.Patch.Select.MultiEdit
                     return;
                 }
 
-                foreach (ConditionalInfo info in scnEditor.instance.allConditionals.Zip(__instance.conditionalButtons, (c, b) => new ConditionalInfo(b, c)))
-                {
-                    ConditionalUtil.MultiEditUsageType multiEditUsageType = default;
-                    ConditionalUtil.UsageType usageType = default;
-
-                    multiEditUsageType = info.Conditional.gid.IsNullOrEmpty()
-                        ? ConditionalUtil.IsUsedMultiEdit(info.Conditional.id, out usageType)
-                        : ConditionalUtil.IsUsedMultiEdit(info.Conditional.gid, out usageType);
-
-                    info.Button.Selected = multiEditUsageType == ConditionalUtil.MultiEditUsageType.UsedByAll;
-                    info.Button.Negated = usageType == ConditionalUtil.UsageType.Negated;
-
-                    Color color = usageType switch
-                    {
-                        ConditionalUtil.UsageType.Normal => InspectorUtil.ConditionalNormalColor,
-                        ConditionalUtil.UsageType.Negated => InspectorUtil.ConditionalNegatedColor,
-                        ConditionalUtil.UsageType.Mixed => InspectorUtil.ConditionalMixedColor,
-                        _ => InspectorUtil.ConditionalUnusedColor
-                    };
-                    
-                    if (multiEditUsageType == ConditionalUtil.MultiEditUsageType.UsedBySome)
-                    {
-                        info.Button.outline.SetActive(true);
-
-                        GameObject parent = new($"Mod_{MyPluginInfo.PLUGIN_GUID}_ConditionalButtonMask");
-                        Image image = parent.AddComponent<Image>();
-                        image.type = Image.Type.Tiled;
-                        image.sprite = ConditionalUtil.UsedBySomeMaskSprite;
-                        image.raycastTarget = false;
-
-                        Mask mask = parent.AddComponent<Mask>();
-                        mask.showMaskGraphic = false;
-
-                        RectTransform transform = parent.transform as RectTransform;
-                        RectTransform imageTransform = info.Button.outlineImage.transform as RectTransform;
-
-                        transform.SetParent(imageTransform.parent);
-                        transform.localPosition = Vector3.zero;
-                        transform.localScale = Vector3.one;
-                        transform.localRotation = Quaternion.identity;
-                        transform.anchorMin = imageTransform.anchorMin;
-                        transform.anchorMax = imageTransform.anchorMax;
-                        transform.offsetMin = Vector2.zero;
-                        transform.offsetMax = Vector2.zero;
-
-                        imageTransform.SetParent(transform);
-
-                        info.Button.conditional.onClick.AddListener(() =>
-                        {
-                            image.enabled = false;
-                            mask.enabled = false;
-                        });
-                    }
-
-                    info.Button.outlineImage.color = color;
-                }
+                MultiEditManager.Instance.ApplyConditionalButtonThings(__instance);
             }
-
-            private record struct ConditionalInfo(ConditionalButton Button, Conditional Conditional);
         }
     }
 }
