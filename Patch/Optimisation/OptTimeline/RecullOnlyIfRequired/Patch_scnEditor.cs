cfbedffd63b1462e2dd3356b71f67ef199c95ad6
9thCore <38700349+9thCore@users.noreply.github.com>
2026-03-31 18:29:46 +0300

    Ugly patch: make sure events appear on first load

---
+++ Patch/Optimisation/OptTimeline/RecullOnlyIfRequired/Patch_scnEditor.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RDLevelEditor;
+
+namespace RDEditorPlus.Patch.Optimisation.OptTimeline.RecullOnlyIfRequired
+{
+    internal static class Patch_scnEditor
+    {
+        [HarmonyPatch(typeof(scnEditor), nameof(scnEditor.DecodeData))]
+        private static class DecodeData
+        {
+            private static void Postfix(scnEditor __instance)
+            {
+                foreach (var control in __instance.eventControls)
+                {
+                    control.UpdateUIInternal();
+                }
+            }
+        }
+    }
+}
