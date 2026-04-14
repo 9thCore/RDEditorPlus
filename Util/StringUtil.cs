dece053ace2a35bb0d85faa0008728337367b921
9thCore <38700349+9thCore@users.noreply.github.com>
2026-04-14 22:59:25 +0300

    Add util for printing a float as its raw value

---
+++ Util/StringUtil.cs
@@ -0,0 +1,9 @@
+﻿namespace RDEditorPlus.Util
+{
+    public static class StringUtil
+    {
+        public static string AsRawValue(this float value) => value.ToString(RawValueFormat);
+
+        private static readonly string RawValueFormat = "0." + new string('#', 52);
+    }
+}
